1c4b46aa791109f786e46e36b1c603866a92de8f
Amartya-18 <89408191+Amartya-18@users.noreply.github.com>
2026-08-03 06:13:38 +0530

    Create PatientDetails class for managing patient info
    This module defines the PatientDetails class, which stores patient information and treatment requirements. It includes methods for displaying patient details and setting patient name and age with validation.

---
+++ patient.py
@@ -0,0 +1,53 @@
+# PATIENT MODULE
+# This file contains the PatientDetails class.
+# It stores patient information to select the issue/test for which an appointment is needed.
+# Defining the PatientDetails Class 
+class PatientDetails:
+   patient_id = 1001         # Class Variable for Auto-Generating Patient ID
+
+# Defining the list of treatment requirements 
+treatment_required = ["Acidity","Acid Reflux","Gastric Issues","Bloating",
+                           "Blood Test","KFT","LFT","CBC","Lipid Profile"]
+
+# Constructor for Initializing Patient Details
+# This function has to be called automatically called whenever a new object is created in PatientDetails Case.
+# patient_name, age, gender and treatment_required are provided while creating the Patient object.
+
+def __init__(self, patient_name, age, gender,treatment_required):
+ PatientDetails.patient_id += 1 
+ self.patient_name = patient_name
+ self.age = age
+ self.gender = gender
+ self.treatment_required = treatment_required
+
+# Function to Display Patient Details
+def display_Patient_details(self):
+  print("Patient Details:" self.patient_details)
+  print("Patient ID:",self.patient_id)
+  print("Patient Name:",self.patient_name)
+  print("Age:",self.age)
+  print("Gender:",self.gender)
+  print("Treatment Required:",self.treatment_required)
+
+#  Setter Function for Patient Name
+ def set_Patient_name(self,patient_name): 
+     try: 
+         if len(patient_name) == 0:
+            raise ValueError("Patient name cannot be empty.")
+            self.patient_name = patient_name  
+          except ValueError as e:
+                print( "Error:",e)
+
+# Setter Function for Patient Age
+# This function ensures that the entered age is numeric and greater than zero.
+   def set_Age(self,age):
+      try:
+           age = int(age)
+           if age <= 0:
+           raise ValueError("Age must be greater than zero.")
+           self.age = age
+           except (TypeError, ValueError) as e:
+            print("Error:",e)
+             
+
+   
