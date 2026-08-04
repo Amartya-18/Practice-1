1de646e91513803f45e19d45c5beab2bdb1f0c5d
Amartya-18 <89408191+Amartya-18@users.noreply.github.com>
2026-08-04 07:08:59 +0530

    Create appointment.py

---
+++ appointment.py
@@ -0,0 +1,58 @@
+from patient import PatientDetails
+from doctor import Gastroenterologist, Pathologist
+from datetime import datetime
+
+# CREATING OBJECTS OF DERIVED DOCTOR CLASSES
+gastro_doctor = Gastroenterologist("Dr. Sharma")
+pathology_doctor = Pathologist("Dr. Mehta")
+# GETTING TREATMENT OPTIONS FROM THE DOCTOR CLASSES
+treatment_options_gastroenterology = (Gastroenterologist.handled_treatments)
+treatment_options_pathology = (Pathologist.handled_treatments)
+
+# Combining Treatment Options of Both Specialities
+treatment_options = (treatment_options_gastroenterology+treatment_options_pathology) 
+# FUNCTION TO DISPLAY AND SELECT TREATMENT REQUIRED
+def choose_Treatment():
+    print("\nAvailable Treatment Requirements:")
+  for treatment in treatment_options:
+      print(treatment)
+  treatment_required = input("\nEnter Treatment Required: ")
+
+    if treatment_required in treatment_options:
+         return treatment_required
+   else:
+          print("Please enter a valid treatment.")
+          return None
+# Mapping Treatment & Doctor Speciality 
+def find_Doctor(treatment_required):
+   if treatment_required in treatment_options_gastroenterology:
+         return gastro_doctor
+      elif treatment_required in treatment_options_pathology:
+           return pathology_doctor
+ else:
+       return None
+
+# Function to get appointment date 
+
+def get_Appointment_date():
+ try:
+       date_input = input("\nEnter Appointment Date (DD-MM-YYYY):")
+       appointment_date = datetime.strptime(date_input,"%d-%m-%Y")
+
+        return appointment_date
+
+ except ValueError:
+
+        print("Please enter a valid date in DD-MM-YYYY format.")
+      return None
+
+# Functions to find available time slots 
+
+def find_Available_slots(doctor,appointment_date):
+      available_slots = []
+
+# Checking Whether the Doctor is Busy 
+
+# FUNCTION TO DISPLAY AND SELECT APPOINTMENT SLOT
+
+  
