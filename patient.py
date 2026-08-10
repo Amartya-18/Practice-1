0f12b4c07210ee6b2fba01c9ee38f5af47086278
Amartya-18 <89408191+Amartya-18@users.noreply.github.com>
2026-08-10 07:55:59 +0530

    Update patient.py

--- patient.py
+++ patient.py
@@ -1,14 +1,14 @@
 # PATIENT FILE
 # This file contains the PatientDetails class.
-# It stores patient information to select the issue/test for which an appointment is needed.
+# It stores patient information  about the patient and the treatment required by the patient. 
 # Defining the PatientDetails Class 
 class PatientDetails:
    patient_id = 1001         # Class Variable for Auto-Generating Patient ID
 
 def __init__(self, patient_name, age, gender,treatment_required):    # Constructor for Initializing Patient Details
-# This function has to be called automatically called whenever a new object is created in PatientDetails Case.
-# patient_name, age, gender and treatment_required are provided while creating the Patient object.
-
+# This function (parameterized constructor) has to be called automatically called whenever a new object is created in PatientDetails Case.
+# Patient_name, age, gender and treatment_required are provided while creating the Patient object.
+  
  PatientDetails.patient_id += 1 
  self.patient_name = patient_name
  self.age = age
@@ -17,7 +17,7 @@ def __init__(self, patient_name, age, gender,treatment_required):    # Construct
 
 # Function to Display Patient Details
 def display_Patient_details(self):
-  print("Patient Details:" self.patient_details)
+  print("\n Patient Details:" self.patient_details)
   print("Patient ID:",self.patient_id)
   print("Patient Name:",self.patient_name)
   print("Age:",self.age)
@@ -43,4 +43,17 @@ def display_Patient_details(self):
            self.age = age
            except (TypeError, ValueError) as e:
             print("Error:",e)
-             
+
+# Setter Function for Patient Age 
+ # This function ensures that age is numeric and greater than zero.
+
+def set_Age(self,age):
+   try:
+         age = int(age)
+         if age <= 0:
+              raise ValueError("Age must be greater than zero.")
+               self.age = age
+    except (TypeError, ValueError) as e:
+           print("Error:",e)
+
+
