6c3c3afe5c3307da42dcb9c9e8557584df167819
Amartya-18 <89408191+Amartya-18@users.noreply.github.com>
2026-08-11 07:17:18 +0530

    Update Appointment_Scheduling_System.py

--- Appointment_Scheduling_System.py
+++ Appointment_Scheduling_System.py
@@ -31,7 +31,10 @@ pat1 = PatientDetails
 pat1.display_Patient_details()         # Diplay Patient Details 
 
 # Finding Suitable Doctor 
-doctor1 = find_Doctor(pat1.treatment_required)
+doctor1 = find_Doctor(pat1.treatment_required)            
+# Association between Doctor & Patient. 
+#  The Patient and Doctor are separate objects, but they interact during the appointment booking process.
+
 # Display Doctor Details 
 doctor1.display_Doctor_details()       
 
@@ -51,7 +54,7 @@ selected_slot_number = int(input("\nEnter Appointment Slot Number: "))
 selected_slot = available_slots[selected_slot_number - 1]
 
 # Booking the Appointment 
-doctor1.book_Appointment(selected_slot)
+doctor1.book_Appointment(selected_slot)                      # Asscoiating the selected slot with doctor 
 
 # Displaying Appointment Confirmation 
 print("\nAppointment Confirmed")
@@ -69,6 +72,19 @@ print("Appointment Date:",selected_slot.strftime
 print("Appointment Time:",selected_slot.strftime
       ("%H:%M hrs")
       )
+
+appointment_details = 
+{
+    "Patient ID": pat1.patient_id,
+    "Patient Name": pat1.patient_name,
+    "Treatment Required": pat1.treatment_required,
+    "Doctor ID": doctor1.doctor_id,
+    "Doctor Name": doctor1.doctor_name,
+    "Treatment Speciality": doctor1.treatment_speciality,
+    "Appointment Date": selected_slot.strftime("%d-%m-%Y"),
+    "Appointment Time": selected_slot.strftime("%H:%M hrs")
+}
+
 
 # File Handling - Serialization
 # The appointment_details dictionary is converted into a storable form and written into appointments.dat.
