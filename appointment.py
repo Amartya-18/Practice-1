e17b36caab62d5fda173be263f028d5eb3444e83
Amartya-18 <89408191+Amartya-18@users.noreply.github.com>
2026-08-11 06:04:11 +0530

    Update appointment.py

--- appointment.py
+++ appointment.py
@@ -1,6 +1,8 @@
-from patient import PatientDetails
-from doctor import Gastroenterologist, Pathologist
-from datetime import datetime
+# Appointment File
+# This file connects the Patient and Doctor classes.
+from patient import PatientDetails                      # Importing Patient Details from patient.py
+from doctor import Gastroenterologist, Pathologist      # Importing the derived doctor classes from doctor.py
+from datetime import datetime                           # Importing date and time for Appointment date
 
 # Creating Objects of derived Doctor Classes
 gastro_doctor = Gastroenterologist("Dr. Sharma")
@@ -11,48 +13,75 @@ treatment_options_gastroenterology = (Gastroenterologist.provided_treatments)
 treatment_options_pathology = (Pathologist.provided_treatments)
 
 # Combining Treatment Options of Both Specialities
-treatment_options = (treatment_options_gastroenterology+treatment_options_pathology) 
+treatment_options = (treatment_options_gastroenterology+
+                     treatment_options_pathology
+                    )
 
 # Function to display and select treatment required 
+
 def choose_Treatment():
     print("\nAvailable Treatment Requirements:")
-  for treatment in treatment_options:
+  
+for treatment in treatment_options:
       print(treatment)
   treatment_required = input("\nEnter Treatment Required: ")
 
+ # Checking Whether Entered Treatment Exists in the List
     if treatment_required in treatment_options:
          return treatment_required
    else:
           print("Please enter a valid treatment.")
           return None
        
-# Mapping Treatment & Doctor Speciality 
+# MFunction to map a treatment to doctor 
 def find_Doctor(treatment_required):
-   if treatment_required in treatment_options_gastroenterology:
+   if treatment_required in treatment_options_gastroenterology:           # Checking Gastroenterology Treatments
          return gastro_doctor
-      elif treatment_required in treatment_options_pathology:
+      elif treatment_required in treatment_options_pathology:             # Checking Pathology Treatments
            return pathology_doctor
  else:
-       return None
+       return None                                                        # In case no matching doctor is found. 
 
 # Function to get appointment date 
 
 def get_Appointment_date():
  try:
        date_input = input("\nEnter Appointment Date (DD-MM-YYYY):")
-       appointment_date = datetime.strptime(date_input,"%d-%m-%Y")
+  # Converting String into datetime Object
+       appointment_date = datetime.strptime
+      (
+           date_input,
+           "%d-%m-%Y"
+       )
        return appointment_date
 except ValueError:
       print("Please enter a valid date in DD-MM-YYYY format.")
       return None
 
-# Functions to find available time slots 
+# Function to display and select Appointment Slot -The Doctor class calculates the available slots.
+# This function only handles the patient's selection.
 
-def find_Available_slots(doctor,appointment_date):  #need to call slots_availability method from doctor class
-      available_slots = [] 
+def choose_Appointment_slot(doctor,appointment_date):
+    
+# Asking the Selected Doctor for Available Slots
+available_slots = doctor.show_Available_slots(appointment_date)
+# Checking Whether Any Slots are Available
+  if len(available_slots) == 0:
+      return None
 
-# Checking Whether the Doctor is Busy ----> no need for this as we are already checking available slots in find_Available_slots method
+# Taking Appointment Slot Number from Patient
+try:
+      selected_slot_number = int(input("\nEnter Appointment Slot Number: "))
+# Checking Whether the Entered Number is Valid
+    if ( 
+       selected_slot_number >= 1
+        and selected_slot_number <= len(available_slots)
+        ):
+# Converting Patient's Number into List Index 
+ selcted_slot = available_slots[selected_slot_number - 1]
+  return selected_slot
 
-# FUNCTION TO DISPLAY AND SELECT APPOINTMENT SLOT  ------> no need to display as we are going to do it in slots_availability method in doctor class
+   except ValueError:
+          print("Please enter a valid number.")
+          return None
 
-  
